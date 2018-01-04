facc68b2d943b08f5bc2d5a97bbbe74d092f24ff
y0m0 <lixi.daniele@gmail.com>
2018-01-04 12:54:20 +0100

    Create dummy reducer and pass it to createStore

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Manager
