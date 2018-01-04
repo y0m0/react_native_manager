84e7ad24d274fcab5f90f1faa7da5ed53711d607
y0m0 <lixi.daniele@gmail.com>
2018-01-04 18:12:42 +0100

    Add and config firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import * as firebase from 'firebase';
 import reducers from './reducers';
 
-export default class App extends Component<{}> {
+const firebaseConfig = {
+  apiKey: 'AIzaSyBCxENjuOI9x8GJj6g20EjInjAGWJPSTI4',
+  authDomain: 'rn-manager-810f5.firebaseapp.com',
+  databaseURL: 'https://rn-manager-810f5.firebaseio.com',
+  projectId: 'rn-manager-810f5',
+  storageBucket: '',
+  messagingSenderId: '1079510736988'
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
