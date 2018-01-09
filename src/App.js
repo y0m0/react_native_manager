cea170a45bf9e99b8b80312b447b47708f7f1a97
y0m0 <lixi.daniele@gmail.com>
2018-01-09 00:00:01 +0000

    Use redux thunk to asynchronously dispatch the login action

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import * as firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
@@ -19,8 +19,9 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component {
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
